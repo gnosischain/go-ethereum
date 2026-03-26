344eb4f02fa47fc1d70d3674f46714b11995ff0b
Aditya <14364734+AdityaSripal@users.noreply.github.com>
2026-03-26 11:42:47 +0100

    Update consensus/aura/aura.go

--- consensus/aura/aura.go
+++ consensus/aura/aura.go
@@ -1185,7 +1185,6 @@ func (f *RollingFinality) buildAncestrySubChain(get func(hash common.Hash) ([]co
 // COPIED FROM CONSENSUS/CLIQUE TO AVOID AN IMPORT CYCLE (clique <- core <- aura <- clique)
 
 // sealHash computes the hash of a header prior to it being sealed.
-// Copied from consensus/clique to avoid an import cycle (clique <- core <- aura <- clique).
 func sealHash(header *types.Header) (hash common.Hash) {
 	hasher := sha3.NewLegacyKeccak256()
 	encodeSigHeader(hasher, header)
